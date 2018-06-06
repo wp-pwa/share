f469eedba930d9631416c54862caf5665a4486aa
David Arenas <darensr@gmail.com>
2018-06-06 13:49:23 +0200

    Changes build folder from 'build' to 'lib'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { types } from 'mobx-state-tree';
 
+const something = { a: 1 };
+
 export default types
   .model('Share')
   .props({
     isWorking: true,
+    something: types.optional(types.frozen, { ...something }),
   });
