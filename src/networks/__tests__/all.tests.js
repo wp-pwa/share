ce5c703180d6695a9ba64618b449919fe9e2f8f8
David Arenas <darensr@gmail.com>
2018-06-08 10:51:34 +0200

    Better code for all.tests.js

--- src/networks/__tests__/all.tests.js
+++ src/networks/__tests__/all.tests.js
@@ -3,52 +3,41 @@ import { set } from 'mobx';
 import All from '../all';
 import Network from '../network';
 
+let share;
+beforeEach(() => {
+  share = types
+    .model('Share')
+    .props({
+      all: types.optional(All, {}),
+      network1: types.optional(Network, {}),
+      network2: types.optional(Network, {}),
+    })
+    .views(self => ({
+      get networks() {
+        return [self.network1, self.network2];
+      },
+    }))
+    .create({});
+    unprotect(share);
+});
+
 describe('Share > All', () => {
   test('count', () => {
-    const share = types
-      .model('Share')
-      .props({
-        all: types.optional(All, {}),
-        network1: types.optional(Network, {}),
-        network2: types.optional(Network, {}),
-      })
-      .views(self => ({
-        get networks() {
-          return [self.network1, self.network2];
-        },
-      }))
-      .create({});
-
-    unprotect(share);
-
     expect(share.all.count({ type: 'post', id: 60 })).toBe(null);
 
-    set(share.network1.countsMap, { post_60: 123 });
-    expect(share.network1.count({ type: 'post', id: 60 })).toBe(123);
-    expect(share.all.count({ type: 'post', id: 60 })).toBe(123);
+    set(share.network1.countsMap, { post_60: 100 });
+    expect(share.network1.count({ type: 'post', id: 60 })).toBe(100);
+    expect(share.all.count({ type: 'post', id: 60 })).toBe(100);
 
-    set(share.network2.countsMap, { post_60: 111 });
-    expect(share.network2.count({ type: 'post', id: 60 })).toBe(111);
-    expect(share.all.count({ type: 'post', id: 60 })).toBe(234);
+    set(share.network2.countsMap, { post_60: 200 });
+    expect(share.network2.count({ type: 'post', id: 60 })).toBe(200);
+    expect(share.all.count({ type: 'post', id: 60 })).toBe(300);
   });
 
   test('requestCount', async () => {
     const requestCount = jest.fn();
-    const NetworkMock = Network.actions(() => ({ requestCount }));
-
-    const share = types
-      .model('Share')
-      .props({
-        all: types.optional(All, {}),
-        network1: types.optional(NetworkMock, {}),
-        network2: types.optional(NetworkMock, {}),
-      })
-      .views(self => ({
-        get networks() {
-          return [self.network1, self.network2];
-        },
-      }))
-      .create({});
+    share.network1.requestCount = requestCount;
+    share.network2.requestCount = requestCount;
 
     share.all.requestCount({ type: 'post', id: 60 });
     expect(requestCount.mock.calls.length).toBe(share.networks.length);
