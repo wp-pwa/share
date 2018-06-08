e19c506c08d0d03c2cd24bc697f5b7333b7a1ecd
David Arenas <darensr@gmail.com>
2018-06-08 11:57:13 +0200

    Adds GooglePlus to Share

--- src/networks/index.js
+++ src/networks/index.js
@@ -1,9 +1,11 @@
 import { types } from 'mobx-state-tree';
 import All from './all';
 import Facebook from './facebook';
+import GooglePlus from './google-plus';
 
 export const all = types.optional(All, {});
 
 export default {
   facebook: types.optional(Facebook, {}),
+  googlePlus: types.optional(GooglePlus, {}),
 };
