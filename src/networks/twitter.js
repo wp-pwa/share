f209320bef782b3007385b7452fe919bf4a16897
David Arenas <darensr@gmail.com>
2018-06-11 19:33:12 +0200

    Fixes url generation for telegram, twitter and whatsapp

--- src/networks/twitter.js
+++ src/networks/twitter.js
@@ -4,16 +4,17 @@ import Network from './network';
 export default Network.named('Twitter').views(self => ({
   url({ type, id, text, via, hashtags }) {
     const url = self.entityLink({ type, id });
+
+    const query = { url };
+    if (text) query.text = text;
+    if (via) query.via = via;
+    if (hashtags) query.hashtags = hashtags.join(',');
+
     return format({
       protocol: 'https:',
       host: 'twitter.com',
       pathname: 'share',
-      query: {
-        url,
-        text,
-        via,
-        hashtags: hashtags.join(','),
-      },
+      query,
     });
   },
 }));
